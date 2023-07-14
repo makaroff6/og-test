e098a7855946b62f7719ab38955fd7ca60410b48
Polina Chernova <73716453+Fire-aqua@users.noreply.github.com>
2023-07-14 12:54:31 +0400

    Update and rename ToSoldAdapter.php to ToSoldBehaviour.php

--- app/Adapters/ToSoldBehaviour.php
+++ app/Adapters/ToSoldBehaviour.php
@@ -4,10 +4,10 @@ namespace App\Adapters;
 
 use App\Enums\GoodStatus;
 use App\Models\Good;
-use App\Strategies\GoodStatusMoveInterface;
+use App\Interfaces\GoodStatusInterface;
 use Exception;
 
-class ToSoldAdapter extends GoodStatusMoveInterface
+class GoodStatusInterface extends GoodStatusInterface
 {
   public GoodStatus $statusTo = GoodStatus::SOLD;
 
