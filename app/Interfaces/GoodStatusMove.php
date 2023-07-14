4618209c438556cc0ad99988297cf50134dea73e
Polina Chernova <73716453+Fire-aqua@users.noreply.github.com>
2023-07-14 12:14:50 +0400

    Update GoodStatusMove.php

--- app/Interfaces/GoodStatusMove.php
+++ app/Interfaces/GoodStatusMove.php
@@ -1,1 +1,14 @@
 
+<?php
+
+namespace App\Interfaces;
+
+use App\Models\Good;
+
+class GoodStatusMove
+{
+  public function execute(GoodStatusMoveInterface $strategy, Good $good): void 
+  {
+    $strategy->execute($good);
+  }
+}
